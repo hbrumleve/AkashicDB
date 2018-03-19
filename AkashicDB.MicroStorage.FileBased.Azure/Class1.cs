81765347423c48f0f388f41c97c572b19782f2d1
Harry Brumleve <hbrumleve@gmail.com>
2018-03-19 14:40:51 -0600

    Setting up Microstorage

---
+++ AkashicDB.MicroStorage.FileBased.Azure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AkashicDB.MicroStorage.FileBased.Azure
+{
+    public class Class1
+    {
+    }
+}
---
+++ AkashicDB.MicroStorage.FileBased/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AkashicDB.MicroStorage.FileBased
+{
+    public class Class1
+    {
+    }
+}
